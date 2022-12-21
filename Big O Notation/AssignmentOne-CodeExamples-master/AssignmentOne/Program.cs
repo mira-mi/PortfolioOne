780beda150f069ff6fdc813802e1ed0ed1e30a98
mira-mi <67883744+mira-mi@users.noreply.github.com>
2022-12-21 10:22:25 -0600

    Add files via upload

---
+++ Big O Notation/AssignmentOne-CodeExamples-master/AssignmentOne/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        //linear
+        int[] intArray2 = new int[5] { 1, 2, 3, 4, 5 };
+        void PrintAllItems(int[] intArray2)
+        {
+            foreach (var item in intArray2)
+            {
+                Console.WriteLine(intArray2);
+            }
+        }
+
+        Random random = new Random();
+        for (int y = 0; y < 5; y++)
+        {
+            Console.WriteLine(random.Next(7));
+            //returns values less than 7
+            //it prints numbers less than 7 and it could also be seven so the entire amount have 7 items
+            //it can only print 7 items
+        }
+
+
+
+        //quadratic
+        int[] intArray3 = new int[5] { 10, 20, 30, 40, 55 };
+        void PrintAllPossibleOrderedPairs(int[] intArray3)
+        {
+            foreach (var firstItem in intArray3)
+            {
+                foreach (var secondItem in intArray3)
+                {
+                    Console.WriteLine($"{firstItem}, {secondItem}");
+                }
+            }
+
+        }
+
+        int h = 7, z = 11;
+        while (h < 10)
+        {
+            Console.WriteLine("First Numeber = {0}", h);
+            h++;
+            while (z < 14)
+            {
+                Console.WriteLine("Second Numebr = {0}", z);
+                z++;
+            }
+            //prints out two numbers in a nested loop, which gives a total of n^2 prints
+            //It shows the quadractic run time that we want to portray those n^2
+        }
+        
+
+
+        //constant
+        int[] intArray4 = new int[7] { 2, 5, 7, 9, 11, 13, 15 };
+        void PrintFirstItem(int[] intArray4)
+        {
+            Console.WriteLine(intArray4[0]);
+        }
+
+        bool IsOdd(int number)
+        {
+            return number % 2 == 0;
+            //prints out an odd number
+            //it can either be 11 or 10001
+            //but as we can see it needs one step to do it's job
+        }
+    }
+
+}
---
+++ Big O Notation/AssignmentOne-CodeExamples-master/README.md
@@ -0,0 +1,24 @@
+# AssignmentOne-CodeExamples
+This is the first code assignment, and here we learned about big o notation
+#Linear notation
+In that one we showed two parts that convey the same thing
+> the first one shows an array with one loop, that one loop prints out 5 items
+-It's pretty basic with the array having the numbers 1-5
+-The code has a pretty standard loop
+>the second one is a random number generator 
+-it has a basic amount that it could choose from
+-it has seven numbers it could use and print out
+#Quadratic notation
+>the first one has nested loops to print out something 
+-It uses a random array to print things out
+-It has one array and it will print out random pairs
+>the second one has nested loops as well
+-It has two arrays to print out multiple things
+-It's kinda like coordinates
+#Constant notation
+>the first one has a a write line condition 
+-it uses an array and it's one step
+-prints out one item
+>the second deals with odd numbers
+-It does one thing as a constant 
+-It has one the capability to print out 1 or 10001 possibilties
